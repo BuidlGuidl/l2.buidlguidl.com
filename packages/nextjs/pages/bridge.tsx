524dcc0972d9417f9f143b8db9741dbc3e55f0fa
Carlos Sánchez <oceanrdn@gmail.com>
2023-09-01 13:44:13 +0200

    Move Bridge to a new page

--- packages/nextjs/pages/bridge.tsx
+++ packages/nextjs/pages/bridge.tsx
@@ -1,22 +1,21 @@
 import type { NextPage } from "next";
 import { MetaHeader } from "~~/components/MetaHeader";
-import AddBGChain from "~~/components/bgChain/AddBGChain";
+import BridgeToBGChain from "~~/components/bgChain/BridgeToBGChain";
 
-const Home: NextPage = () => {
+const Bridge: NextPage = () => {
   return (
     <>
       <MetaHeader />
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center mb-8">
-            <span className="block text-4xl font-bold">BuidlGuidl L2</span>
+            <span className="block text-4xl font-bold">Bridge Sepolia ETH to BG L2 Chain</span>
           </h1>
-          <p className="text-center text-lg">L2 Tesnet (powered by the OP stack) for the BuidlGuidl.</p>
-          <AddBGChain />
+          <BridgeToBGChain />
         </div>
       </div>
     </>
   );
 };
 
-export default Home;
+export default Bridge;
